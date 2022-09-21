47ab48b778b6353df1162c2ee4052c9e533fcb08
Michal Nevo <nevosmic28@gmail.com>
2022-09-21 13:46:06 +0300

    change table and render template

--- app/app.py
+++ app/app.py
@@ -1,23 +1,25 @@
 from typing import List, Dict
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 import json
+import os
 
 app = Flask(__name__)
-
-
-def test_table() -> List[Dict]:
+print("PWDDDDDDDDDD")
+print(os.getcwd())
+def csv_table():
     config = {
         'user': 'root',
         'password': 'root',
         'host': 'db',
         'port': '3306',
-        'database': 'devopsroles'
+        'database': 'attendance_db'
     }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM test_table')
-    results = [{name: color} for (name, color) in cursor]
+    cursor.execute('SELECT * FROM csv_table')
+    
+    results = [{id:Meeting_start_time} for (id, Meeting_start_time) in cursor]
     cursor.close()
     connection.close()
 
@@ -26,7 +28,22 @@ def test_table() -> List[Dict]:
 
 @app.route('/')
 def index() -> str:
-    return json.dumps({'test_table': test_table()})
+    # get students from db:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'attendance_db'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM csv_table')
+    students = cursor.fetchall()
+    print("STUDENTS")
+    print(students)
+    return render_template('students.html', data=students)
+
 
 
 if __name__ == '__main__':
--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,14 @@
-create database devopsroles;
-use devopsroles;
+create database attendance_db;
+use attendance_db;
 
-CREATE TABLE test_table (
-  name VARCHAR(20),
-  color VARCHAR(10)
+CREATE TABLE csv_table (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(10) NOT NULL,
+  Meeting_start_time TIMESTAMP NOT NULL
 );
 
-INSERT INTO test_table
-  (name, color)
+INSERT INTO csv_table
+  (Name, Meeting_start_time)
 VALUES
-  ('dev', 'blue'),
-  ('pro', 'yellow');
+  ("Michal", "2022-08-18 15:53:47"),
+  ("Keren", "2022-08-18 15:53:47");
