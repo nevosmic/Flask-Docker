5e40ea620ce63123b6b54c89cf9210acaa4737fd
Michal Nevo <nevosmic28@gmail.com>
2022-09-22 17:31:17 +0300

    db connection seperate file

--- app/app.py
+++ app/app.py
@@ -2,25 +2,19 @@ from typing import List, Dict
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 #from dotenv import load_dotenv
+from db_connection import *
 from sftp_connection import *
 import json
 import os
 
 app = Flask(__name__)
 sftp_get_files()
-config = {
-        'user': os.environ.get("MYSQL_USER"),
-        'password': os.environ.get("MYSQL_PASSWORD"),
-        'host': os.environ.get("MYSQL_HOST"),
-        'port': '3306',
-        'database': os.environ.get("MYSQL_DB")
-}
-
+connection, cursor = get_connection_to_mysql()
 #load_dotenv()
 @app.route('/')
 def index():
     # get students from db:
-    connection = mysql.connector.connect(**config)
+    
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM csv_table')
     students = cursor.fetchall()
---
+++ app/db_connection.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import os
+def get_connection_to_mysql():
+    config = {
+        'user': os.environ.get("MYSQL_USER"),
+        'password': os.environ.get("MYSQL_PASSWORD"),
+        'host': os.environ.get("MYSQL_HOST"),
+        'port': '3306',
+        'database': os.environ.get("MYSQL_DB")
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute("select database();")
+    db = cursor.fetchone()
+
+    if db:
+        print("You're connected to database: ", db)
+    else:
+        print('Not connected.')
+    return connection, cursor
+
