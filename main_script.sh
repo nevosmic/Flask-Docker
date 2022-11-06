30082c041ec9b5b102496233a19c997162680454
nevosmic <nevosmic28@gmail.com>
2022-11-06 15:55:06 +0000

    whoami

--- main_script.sh
+++ main_script.sh
@@ -2,8 +2,8 @@ echo "Connect to test server"
 echo "Creating project dir"
 SSH_TEST_KEY=/var/lib/jenkins/.ssh/test_key
 JENKINS_WORKSPACE=/var/lib/jenkins/workspace/git and docker connection
-ssh -i $SSH_TEST_KEY -o StrictHostKeyChecking=no test "mkdir -p /home/ec2-user/final-proj"
+ssh -i $SSH_TEST_KEY ec2-user@test "mkdir -p /home/ec2-user/final-proj"
 scp $JENKINS_WORKSPACE/docker-compose.yml ec2-user@test:/home/ec2-user/final-proj/docker-compose.yml
 scp $JENKINS_WORKSPACE/.env ec2-user@test:/home/ec2-user/final-proj/.env
-ssh -i $SSH_TEST_KEY -o StrictHostKeyChecking=no test "ls -a"
-ssh -i $SSH_TEST_KEY -o StrictHostKeyChecking=no test "cd /home/ec2-user/final-proj;docker-compose up --no-build -d;sleep 20;docker container ls; docker-compose down"
+ssh -i $SSH_TEST_KEY ec2-user@test "ls -a"
+ssh -i $SSH_TEST_KEY ec2-user@test "cd /home/ec2-user/final-proj;docker-compose up --no-build -d;sleep 20;docker container ls; docker-compose down"
