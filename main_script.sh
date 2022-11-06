1fef47ff627fc2fec2e8ca7309bb295b0a4b5ae3
nevosmic <nevosmic28@gmail.com>
2022-11-06 13:05:06 +0000

    main script first commit

--- main_script.sh
+++ main_script.sh
@@ -1,7 +1,8 @@
 echo "Connect to test server"
 echo "Creating project dir"
+SSH_KEY=/home/ec2-user/.ssh/id_dsa
 JENKINS_WORKSPACE=/var/lib/jenkins/workspace/git and docker connection
-ssh -i .ssh/id_dsa -o StrictHostKeyChecking=no test "mkdir -p /home/ec2-user/final-proj"
+ssh -i $SSH_KEY -o StrictHostKeyChecking=no test "mkdir -p /home/ec2-user/final-proj"
 scp $JENKINS_WORKSPACE/docker-compose.yml ec2-user@test:/home/ec2-user/final-proj/docker-compose.yml
 scp $JENKINS_WORKSPACE/.env ec2-user@test:/home/ec2-user/final-proj/.env
 ssh -i .ssh/id_dsa -o StrictHostKeyChecking=no test "ls -a"
