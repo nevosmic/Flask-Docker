702c37e04f28367e0b9bc33959318deab5894a91
nevosmic <nevosmic28@gmail.com>
2022-11-06 14:05:38 +0000

    ssh key

--- main_script.sh
+++ main_script.sh
@@ -2,8 +2,8 @@ echo "Connect to test server"
 echo "Creating project dir"
 SSH_KEY=/var/lib/jenkins/.ssh/id_dsa
 JENKINS_WORKSPACE=/var/lib/jenkins/workspace/git and docker connection
-ssh -i $SSH_KEY -o StrictHostKeyChecking=no test "mkdir -p /home/ec2-user/final-proj"
+ssh -i "/home/ec2-user/.ssh/id_dsa" -o StrictHostKeyChecking=no test "mkdir -p /home/ec2-user/final-proj"
 scp $JENKINS_WORKSPACE/docker-compose.yml ec2-user@test:/home/ec2-user/final-proj/docker-compose.yml
 scp $JENKINS_WORKSPACE/.env ec2-user@test:/home/ec2-user/final-proj/.env
-ssh -i .ssh/id_dsa -o StrictHostKeyChecking=no test "ls -a"
-ssh -i .ssh/id_dsa -o StrictHostKeyChecking=no test "cd /home/ec2-user/final-proj;docker-compose up --no-build -d;sleep 20;docker container ls; docker-compose down"
+ssh -i "/home/ec2-user/.ssh/id_dsa" -o StrictHostKeyChecking=no test "ls -a"
+ssh -i "/home/ec2-user/.ssh/id_dsa" -o StrictHostKeyChecking=no test "cd /home/ec2-user/final-proj;docker-compose up --no-build -d;sleep 20;docker container ls; docker-compose down"
