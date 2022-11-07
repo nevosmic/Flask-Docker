44a47b6ccc50cbfe8d441de5ffaa5aab1c1e7272
nevosmic <nevosmic28@gmail.com>
2022-11-07 12:13:58 +0000

    change format deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
-# VARS
-HOME_DIR="/home/ec2-user"
-JEMKINS_WORKSPACE="/var/lib/jenkins/workspace/Bynet_attendance"
 INPUT=$1
 echo "Connect to $1 server"
-echo "Creating project dir"
-ssh ec2-user@test "hostname;mkdir -p /home/ec2-user/final;ls -la;"
-echo "Copying docker-compose file to test"
-scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final/docker-compose.yml
-scp /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final/.env
-ssh ec2-user@test "cd /home/ec2-user/final;ls -la;"
-ssh ec2-user@test "pwd;cd /home/ec2-user/final;pwd;docker-compose up -d;sleep 100;docker container ls;"
-ssh -v ec2-user@test "curl -I localhost:5000"
-echo "done"
+ssh ec2-user@test "mkdir -p /homw/ec2-user/final_proj;ls -la;"
+echo "Copy docker compose file to test"
+scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final_proj/docker-compose.yml
+scp /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final_proj/.env
+ssh ec2-user@test "cd /home/ec2-user/final_proj;ls -la;"
+ssh ec2-user@test "cd /home/ec2-user/final_proj;docker-compose up -d; sleep 100; docker container ls;"
+ssh -v ec2-user@test "curl -I 35.79.222.182:5000"
