01cf0f5ed0c16909de6984a9c8559f053d90ae7b
nevosmic <nevosmic28@gmail.com>
2022-11-07 09:46:01 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ mechine=$1
 
 echo "Connect to $mechine server"
 echo "Creating project dir"
-ssh "ec2-user@$mechine" "mkdir -p $HOME_DIR/final-proj"
+ssh "ec2-user@${mechine}" "mkdir -p ${HOME_DIR}/final-proj"
 echo "Copying docker-compose file to $mechine"
-scp "$JEMKINS_WORKSPACE/docker-compose.yml" "ec2-user@$mechine:$HOME_DIR/final-proj"
+scp "${JEMKINS_WORKSPACE}/docker-compose.yml" "ec2-user@${mechine}:${HOME_DIR}/final-proj"
 echo "Starting project"
-ssh "ec2-user@$mechine" "pwd;cd $HOME_DIR/final-proj;pwd;docker-compose up -d;sleep 30;docker container ls; docker-compose down"
+ssh "ec2-user@${mechine}" "pwd;cd ${HOME_DIR}/final-proj;pwd;docker-compose up -d;sleep 30;docker container ls; docker-compose down"
 echo "END UP"
