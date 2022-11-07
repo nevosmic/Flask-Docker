a21e217af4ef4266498370934d257f85d5c78c0a
nevosmic <nevosmic28@gmail.com>
2022-11-07 12:21:32 +0000

    change format deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ INPUT=$1
 echo "Connect to $1 server"
 ssh ec2-user@test "mkdir -p /home/ec2-user/final_proj;ls -la;"
 echo "Copy docker compose file to test"
-scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final_proj/docker-compose.yml
-scp /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final_proj/.env
+scp -v /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final_proj/docker-compose.yml
+scp -v /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final_proj/.env
 ssh ec2-user@test "cd /home/ec2-user/final_proj;ls -la;"
-ssh ec2-user@test "cd /home/ec2-user/final_proj;docker-compose up -d; sleep 100; docker container ls;"
-ssh -v ec2-user@test "curl -I 35.79.222.182:5000"
+ssh -v ec2-user@test "cd /home/ec2-user/final_proj;docker-compose up -d; sleep 100; docker container ls;"
+ssh ec2-user@test "curl -I 35.79.222.182:5000"
