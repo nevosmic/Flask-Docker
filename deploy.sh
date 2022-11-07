07552cb2e3fe63a570cfa4aa5167935cf620ee95
nevosmic <nevosmic28@gmail.com>
2022-11-07 11:38:51 +0000

    curl

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#VARS
+# VARS
 HOME_DIR="/home/ec2-user"
 JEMKINS_WORKSPACE="/var/lib/jenkins/workspace/Bynet_attendance"
 INPUT=$1
@@ -9,5 +9,5 @@ echo "Copying docker-compose file to test"
 scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final/docker-compose.yml
 scp -v /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final/.env
 ssh ec2-user@test "cd /home/ec2-user/final;ls -la;"
-ssh "ec2-user@test" "pwd;cd /home/ec2-user/final;pwd;docker-compose up -d;sleep 30;docker container ls; docker-compose down;"
+ssh "ec2-user@test" "pwd;cd /home/ec2-user/final;pwd;docker-compose up -d;sleep 90;docker container ls;curl -I localhost:5000;docker-compose down;"
 echo "done"
