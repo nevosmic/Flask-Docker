74f8cf83663f0adc1fec7a5108405d5df5002f90
nevosmic <nevosmic28@gmail.com>
2022-11-07 15:55:05 +0000

    name is app

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 INPUT=$1
 echo "Connect to $1 server"
-ssh ec2-user@test "mkdir -p /home/ec2-user/final_proj;ls -la;"
+ssh ec2-user@test "mkdir -p /home/ec2-user/app;ls -la;"
 echo "Copy docker compose file to test"
-scp -v /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/final_proj/docker-compose.yml
-scp -v /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/final_proj/.env
-ssh -v ec2-user@test "cd /home/ec2-user/final_proj;ls -la;docker-compose up -d --no-build; sleep 160; docker container ls -a;"
+scp -v /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/app/docker-compose.yml
+scp -v /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/app/.env
+ssh -v ec2-user@test "cd /home/ec2-user/app;ls -la;docker-compose up -d --no-build; sleep 160; docker container ls -a;"
 #ssh ec2-user@test "curl -I 35.79.222.182:5000"
