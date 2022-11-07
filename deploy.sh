bad2f0e8f088d588c02afe3a1ef2b68d1873144b
nevosmic <nevosmic28@gmail.com>
2022-11-07 09:55:25 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@ HOME_DIR="/home/ec2-user"
 JEMKINS_WORKSPACE="/var/lib/jenkins/workspace/Bynet_attendance"
 input=$1
 echo "$input"
-mechine=test
+mechine="test"
 echo "Connect to ${mechine} server"
 echo "Creating project dir"
 ssh "ec2-user@$test" "mkdir -p /home/ec2-user/final-proj"
 echo "Copying docker-compose file to test"
-scp "/var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml" "ec2-user@${mechine}:/home/ec2-user/final-proj"
+scp "/var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml" "ec2-user@${mechine}:/home/ec2-user/final-proj/docker-compose.yml"
+scp "/var/lib/jenkins/workspace/Bynet_attendance/.env" "ec2-user@${mechine}:/home/ec2-user/final-proj/.env"
+
 echo "Starting project"
 ssh "ec2-user@test" "pwd;cd /home/ec2-user/final-proj;pwd;docker-compose up -d;sleep 30;docker container ls; docker-compose down"
 echo "END UP"
