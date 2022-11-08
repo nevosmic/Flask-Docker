a8dc42c615699224ee0b4eed90c5033e283ac801
nevosmic <70336936+nevosmic@users.noreply.github.com>
2022-11-08 14:06:39 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ ssh ec2-user@test "mkdir -p /home/ec2-user/app;ls -la;"
 echo "Copy docker compose file to test"
 scp -v /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@test:/home/ec2-user/app/docker-compose.yml
 scp -v /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@test:/home/ec2-user/app/.env
-ssh -v ec2-user@test "cd /home/ec2-user/app;ls -la;docker-compose up -d --no-build; sleep 160; docker container ls -a;"
+ssh -v ec2-user@test "cd /home/ec2-user/app;ls -la;docker pull nevosmic/bynet_docker:v0.6; docker-compose up -d --no-build; sleep 160; docker container ls -a;"
 #ssh ec2-user@test "curl -I 35.79.222.182:5000"
