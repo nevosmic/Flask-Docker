ac9b512373bc72c21231f66d3a2ee7cb87323d8c
nevosmic <nevosmic28@gmail.com>
2022-11-09 18:14:09 +0000

    tag 15

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ scp /home/ec2-user/my_proj1/Flask-Docker.env ec2-user@$MACHINE:/home/ec2-user/fi
 ssh -v ec2-user@$MACHINE "cd /home/ec2-user/final_proj;ls -la;docker pull $IMAGE;docker-compose up -d --no-build;sleep 100;docker container ls -a;"
 ssh -v ec2-user@$MACHINE "curl -Is 35.76.115.227:5000"
 #clean up
-#ssh ec2-user@$MACHINE "docker-compose down;sleep 160;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
+ssh ec2-user@$MACHINE "docker-compose down;sleep 160;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
