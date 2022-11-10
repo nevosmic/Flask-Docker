8305df03b06a2b04f9b2c6a4f8d9bba79da21cda
nevosmic <nevosmic28@gmail.com>
2022-11-10 17:10:31 +0000

    tag 38

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ echo "Copy env file to $MACHINE server"
 scp $JENKINS_WORKSPACE/.env ec2-user@$MACHINE:$WORK_DIR/final_proj/.env
 ssh ec2-user@$MACHINE "cd ${WORK_DIR}/final_proj;ls -la;docker pull $IMAGE;docker-compose up -d --no-build;sleep 30;docker container ls -a;sleep 80;"
 #ssh ec2-user@$MACHINE "curl -Is localhost:5000;"
-if [ $MACHINE == "test" ]
+if [[ $MACHINE == "test" ]]
 then
 	echo "Copy test file to $MACHINE server"
 	scp $JENKINS_WORKSPACE/test.sh ec2-user@$MACHINE:$WORK_DIR/test.sh
 	echo "TESTING.."
-	ssh ec2-user@$MACHINE "ls -la;bash test.sh;"
+	ssh ec2-user@$MACHINE "ls -la;bash -xe test.sh;"
 	echo "Cleaning in TEST server"
 	#clean up
 	ssh -v ec2-user@$MACHINE "cd ${WORK_DIR}/final_proj;docker-compose down;sleep 60;docker container ls -a;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
