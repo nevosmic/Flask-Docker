cb2e90e3d7ee7b1a98255d82cc2a6cb08cdc6fd5
nevosmic <nevosmic28@gmail.com>
2022-11-10 13:56:07 +0000

    REMOVE docker-compose down on prod

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,12 @@ echo $MACHINE
 #JENKINS_WORKSPACE=/var/lib/jenkins/workspace/Bynet_attendance/
 echo "Connect to $MACHINE server"
 ssh ec2-user@$MACHINE "mkdir -p /home/ec2-user/final_proj;ls -la;"
-#ssh ec2-user@$MACHINE "mkdir -p /home/ec2-user/final_proj/app;ls -la;"
 echo "Copy docker compose file to $MACHINE server"
 scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@$MACHINE:/home/ec2-user/final_proj/docker-compose.yml
 echo "Copy env file to $MACHINE server"
 scp /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@$MACHINE:/home/ec2-user/final_proj/.env
 ssh ec2-user@$MACHINE "cd /home/ec2-user/final_proj;ls -la;docker pull $IMAGE;docker-compose up -d --no-build;sleep 30;docker container ls -a;sleep 80;"
-ssh -v ec2-user@$MACHINE "curl -Is localhost:5000;"
+#ssh ec2-user@$MACHINE "curl -Is localhost:5000;"
 if [ $MACHINE == "test" ]
 then
 	echo "Copy test file to $MACHINE server"
@@ -18,9 +17,10 @@ then
 	echo "TESTING.."
 	ssh ec2-user@$MACHINE "ls -la;bash test.sh;"
 	echo "Cleaning in TEST server"
+	#clean up
+	ssh -v ec2-user@$MACHINE "cd /home/ec2-user/final_proj;docker-compose down;sleep 60;docker container ls -a;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
+
 else
-	echo "Cleaning in $MACHINE server"
+	echo "Michal's app is on :)"
 fi
-#clean up
-ssh -v ec2-user@$MACHINE "cd /home/ec2-user/final_proj;docker-compose down;sleep 60;docker container ls -a;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
 
