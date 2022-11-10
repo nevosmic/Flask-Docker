8e35e1e685d376bc2adc48df0c0ec4df25e1a973
nevosmic <nevosmic28@gmail.com>
2022-11-10 12:06:10 +0000

    new test.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 MACHINE=$1
 IMAGE=$2
+#JENKINS_WORKSPACE=/var/lib/jenkins/workspace/Bynet_attendance/
 echo "Connect to $MACHINE server"
-
 ssh ec2-user@$MACHINE "mkdir -p /home/ec2-user/final_proj;ls -la;"
 #ssh ec2-user@$MACHINE "mkdir -p /home/ec2-user/final_proj/app;ls -la;"
 echo "Copy docker compose file to $MACHINE"
 scp /var/lib/jenkins/workspace/Bynet_attendance/docker-compose.yml ec2-user@$MACHINE:/home/ec2-user/final_proj/docker-compose.yml
-scp -v /home/ec2-user/my_proj1/Flask-Docker/.env ec2-user@$MACHINE:/home/ec2-user/final_proj/.env
+scp /var/lib/jenkins/workspace/Bynet_attendance/.env ec2-user@$MACHINE:/home/ec2-user/final_proj/.env
 ssh -v ec2-user@$MACHINE "cd /home/ec2-user/final_proj;ls -la;docker pull $IMAGE;docker-compose up -d --no-build;sleep 30;docker container ls -a;sleep 60;"
-ssh -v ec2-user@$MACHINE "curl -Is 13.230.64.25:5000"
+#ssh -v ec2-user@$MACHINE "curl -Is 13.230.64.25:5000"
+if [$MACHINE -eq "test"]
+then
+scp /var/lib/jenkins/workspace/Bynet_attendance/test.sh ec2-user@$MACHINE:/home/ec2-user/test.sh
+ssh -v ec2-user@$MACHINE "ls -la;bash test.sh;"
+else
+
 #clean up
 ssh -v ec2-user@$MACHINE "cd /home/ec2-user/final_proj;docker-compose down;sleep 60;docker container ls -a;docker system prune --volumes;docker rmi $IMAGE;echo 'FINISH'"
+fi
--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
-echo "In test server"
-IP=curl http://checkip.amazonaws.com
-echo $IP
+#!/bin/bash
+URL=localhost:5000
+RES=`curl -s -I ${URL} | grep HTTP | awk {'print $2'}`
+echo $RES
+if [ $RES -eq 200 ]
+then
+        echo "OK !"
+else
+        echo "TEST NOT PASS"
+        echo $RES
+        exit 1
+fi
+
