b08a3422047ebe6b393409c53cb2dbb33b041f77
nevosmic <nevosmic28@gmail.com>
2022-11-10 13:19:45 +0000

    FIX TEST.SH FORMAT

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 URL=localhost:5000
-RES=`curl -s -I ${URL} | grep HTTP | awk {'print $2'}`
-echo $RES
-if [ $RES -eq 200 ]
+RESULT=`curl -s -I ${URL} | grep HTTP | awk {'print $2'}`
+echo $RESULT
+if [ $RESULT -eq 200 ]
 then
-        echo "OK !"
+	echo " OK !"
 else
-        echo "TEST NOT PASS"
-        echo $RES
-        exit 1
+	echo "TEST NOT PASS !"
+	exit 1
 fi
-
